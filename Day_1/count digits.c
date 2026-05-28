7d19d2bedb40d8a3b71f65e9f85638b9aaa34ef3
Mohneesh-Sharma <vandnasharma031985@gmail.com>
2026-05-28 22:43:29 +0530

    TO COUNT THE NUMBER OF DIGITS

--- Day_1/count digits.c
+++ Day_1/count digits.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main(){
-    int n,count=0;
+    int n,n1,count=0;
     printf("enter a number :");
     scanf("%d",&n);
+    n1=n;
     while(n!=0){
         count++;
         n/=10;
     }
-    printf("number of digits of %d is : %d",n,count);
+    printf("number of digits of %d is : %d",n1,count);
     return 0;
 }
